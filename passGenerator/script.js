f4c8bb175292f1ad13156941c6ceb59957c98f60
Dondaji Rajesh <141136709+rajesh9881@users.noreply.github.com>
2024-03-04 11:40:48 +0530

    Add files via upload

---
+++ passGenerator/script.js
@@ -0,0 +1,43 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+const allchars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (lenght > password.length) {
+        password += allchars[Math.floor(Math.random() * allchars.length)];
+    }
+
+    passwordBox.value = password;
+
+}  
+
+
+function copyPassword() {
+    const passwordBox = document.getElementById("password");
+
+   
+    passwordBox.select();
+    passwordBox.setSelectionRange(0, 99999); 
+
+    
+    document.execCommand("copy");
+
+    
+    window.getSelection().removeAllRanges();
+
+    // To Notify the user that the password has been copied
+    alert("Password copied to clipboard: ");
+}
+
